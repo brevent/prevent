bba02c2a3816f4afad226f71ac8abf67377ed3f2
Liu DongMiao <liudongmiao@gmail.com>
2015-07-13 17:21:27 +0800

    simplify save mode, only system can save packages

--- src/me/piebridge/forcestopgb/hook/Packages.java
+++ src/me/piebridge/forcestopgb/hook/Packages.java
@@ -20,30 +20,27 @@ import me.piebridge.forcestopgb.common.CommonIntent;
 
 final class Packages {
 
-    public static final String FORCESTOP = Environment.getDataDirectory() + "/data/" + CommonIntent.PACKAGE_NAME + "/conf/forcestop.list";
+    public static final String FORCESTOP = Environment.getDataDirectory() + "/data/me.piebridge.forcestopgb/conf/forcestop.list";
 
     public static void save(Map<String, Boolean> packages) {
         android.util.Log.d(CommonIntent.TAG, "save packages");
-        try {
-            File lock = new File(FORCESTOP + ".lock");
-            while (lock.exists() && System.currentTimeMillis() - lock.lastModified() < 3000) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) { // NOSONAR
-                    // do nothing
-                }
+        File lock = new File(FORCESTOP + ".lock");
+        while (lock.exists() && System.currentTimeMillis() - lock.lastModified() < 3000) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { // NOSONAR
+                // do nothing
             }
-            Set<String> keys = new TreeSet<String>(packages.keySet());
+        }
+        Set<String> keys = new TreeSet<String>(packages.keySet());
+        try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(lock));
             for (String key : keys) {
                 writer.write(key);
-                writer.write("=");
-                writer.write(String.valueOf(packages.get(key)));
                 writer.write("\n");
             }
             writer.close();
             lock.renameTo(new File(FORCESTOP));
-            FileUtils.setPermissions(FORCESTOP, 0640, Process.SYSTEM_UID, Process.SYSTEM_UID);
         } catch (IOException e) { // NOSONAR
             // do nothing
         }
@@ -59,13 +56,12 @@ final class Packages {
             }
             BufferedReader reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                String[] components = line.split("=");
-                if (components.length > 1) {
-                    packages.put(components[0], Boolean.valueOf(components[1]));
-                } else {
-                    packages.put(line, Boolean.TRUE);
+                int index = line.indexOf('=');
+                if (index != -1) {
+                    line = line.substring(0, index);
                 }
+                line = line.trim();
+                packages.put(line, Boolean.TRUE);
             }
             reader.close();
         } catch (IOException e) { // NOSONAR
@@ -75,28 +71,22 @@ final class Packages {
     }
 
     public static void resetPackages() {
-        File parent = new File(FORCESTOP).getParentFile();
-        if (parent.isDirectory()) {
-            boolean changed = false;
-            Map<String, Boolean> packages = Packages.load();
-            for (String key : packages.keySet()) {
-                if (!packages.get(key)) {
-                    changed = true;
-                    packages.put(key, Boolean.TRUE);
-                }
-            }
-            if (changed) {
-                Packages.save(packages);
-            } else {
-                FileUtils.setPermissions(FORCESTOP, 0640, Process.SYSTEM_UID, Process.SYSTEM_UID);
-            }
-        } else {
+        File file = new File(FORCESTOP);
+        File parent = file.getParentFile();
+        if (!parent.isDirectory()) {
             if (parent.exists()) {
                 parent.delete();
             }
             parent.mkdir();
-            save(new HashMap<String, Boolean>());
         }
         FileUtils.setPermissions(parent.getAbsolutePath(), 0750, Process.SYSTEM_UID, Process.SYSTEM_UID);
+        if (!file.isFile()) {
+            if (file.exists()) {
+                file.delete();
+            }
+            save(new HashMap<String, Boolean>());
+        }
+        FileUtils.setPermissions(FORCESTOP, 0640, Process.SYSTEM_UID, Process.SYSTEM_UID);
     }
+
 }
