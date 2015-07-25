34c31334139e55c02b4f06222671af1ad69fb981
Liu DongMiao <liudongmiao@gmail.com>
2015-07-25 13:09:05 +0800

    use xposed log to record log

--- src/me/piebridge/prevent/framework/PreventLog.java
+++ src/me/piebridge/prevent/framework/PreventLog.java
@@ -2,6 +2,8 @@ package me.piebridge.prevent.framework;
 
 import android.util.Log;
 
+import de.robv.android.xposed.XposedBridge;
+
 /**
  * Created by thom on 15/7/25.
  */
@@ -19,21 +21,28 @@ public class PreventLog {
 
     public static void d(String msg) {
         Log.d(TAG, msg);
+        XposedBridge.log("[" + TAG + "]" + "[DEBUG]" + msg);
     }
 
     public static void i(String msg) {
-        Log.d(TAG, msg);
+        Log.i(TAG, msg);
+        XposedBridge.log("[" + TAG + "]" + "[ INFO]" + msg);
     }
 
     public static void w(String msg) {
         Log.w(TAG, msg);
+        XposedBridge.log("[" + TAG + "]" + "[ WARN]" + msg);
     }
 
     public static void e(String msg) {
         Log.e(TAG, msg);
+        XposedBridge.log("[" + TAG + "]" + "[ERROR]" + msg);
     }
 
     public static void e(String msg, Throwable t) {
         Log.e(TAG, msg, t);
+        XposedBridge.log("[" + TAG + "]" + "[ERROR]" + msg);
+        XposedBridge.log(t);
     }
+
 }
