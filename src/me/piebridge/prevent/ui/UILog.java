bf38694cb60a67aaeec7cce4c4ee7b81544c9905
Liu DongMiao <liudongmiao@gmail.com>
2015-07-25 13:15:06 +0800

    use xposed to log ui too

--- src/me/piebridge/prevent/ui/UILog.java
+++ src/me/piebridge/prevent/ui/UILog.java
@@ -2,6 +2,8 @@ package me.piebridge.prevent.ui;
 
 import android.util.Log;
 
+import de.robv.android.xposed.XposedBridge;
+
 /**
  * Created by thom on 15/7/25.
  */
@@ -15,17 +17,23 @@ public class UILog {
 
     public static void d(String msg) {
         Log.d(TAG, msg);
-    }
-
-    public static void e(String msg, Throwable t) {
-        Log.e(TAG, msg, t);
+        XposedBridge.log("[" + TAG + "]" + "[DEBUG]" + msg);
     }
 
     public static void d(String msg, Throwable t) {
         Log.d(TAG, msg, t);
+        XposedBridge.log("[" + TAG + "]" + "[DEBUG]" + msg);
     }
 
     public static void i(String msg) {
         Log.i(TAG, msg);
+        XposedBridge.log("[" + TAG + "]" + "[ INFO]" + msg);
     }
+
+    public static void e(String msg, Throwable t) {
+        Log.e(TAG, msg, t);
+        XposedBridge.log("[" + TAG + "]" + "[ERROR]" + msg);
+        XposedBridge.log(t);
+    }
+
 }
