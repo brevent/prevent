ddb328226b246bc409b2c212e8d696955393de2f
Liu DongMiao <liudongmiao@gmail.com>
2015-07-26 14:35:07 +0800

    add notification message

--- src/me/piebridge/prevent/ui/PreventProvider.java
+++ src/me/piebridge/prevent/ui/PreventProvider.java
@@ -1,16 +1,26 @@
 package me.piebridge.prevent.ui;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
 import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Set;
 
 import me.piebridge.prevent.ui.util.PackageUtils;
 import me.piebridge.prevent.ui.util.PreventListUtils;
+
+import me.piebridge.forcestopgb.R;
 
 /**
  * Created by thom on 15/7/18.
@@ -30,7 +40,8 @@ public class PreventProvider extends ContentProvider {
         String[] columns = {COLUMN_PACKAGE};
         MatrixCursor cursor = new MatrixCursor(columns);
         PackageManager pm = getContext().getPackageManager();
-        for (String packageName : PreventListUtils.load()) {
+        Set<String> packages = PreventListUtils.load();
+        for (String packageName : packages) {
             try {
                 ApplicationInfo appInfo = pm.getApplicationInfo(packageName, 0);
                 if (PackageUtils.canPrevent(pm, appInfo)) {
@@ -39,6 +50,9 @@ public class PreventProvider extends ContentProvider {
             } catch (PackageManager.NameNotFoundException e) {
                 UILog.d("cannot find package " + packageName, e);
             }
+        }
+        if (packages.isEmpty()) {
+            notifyNoPrevents(getContext());
         }
         return cursor;
     }
@@ -62,4 +76,22 @@ public class PreventProvider extends ContentProvider {
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         return 0;
     }
+
+    private void notifyNoPrevents(Context context) {
+        Intent open = new Intent(context, PreventActivity.class);
+        open.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent activity = PendingIntent.getActivity(context, 0, open, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(context)
+                .setAutoCancel(true)
+                .setContentTitle(context.getText(R.string.app_name))
+                .setContentText(context.getText(R.string.no_prevents))
+                .setTicker(context.getText(R.string.app_name))
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(activity).build();
+
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(0, notification);
+    }
+
 }
