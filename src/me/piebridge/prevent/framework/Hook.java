106fdf793e0738b4435fa7aafe68f0694e399e2e
Liu DongMiao <liudongmiao@gmail.com>
2015-07-27 00:28:26 +0800

    use application context instead of activity context

--- src/me/piebridge/prevent/framework/Hook.java
+++ src/me/piebridge/prevent/framework/Hook.java
@@ -15,8 +15,12 @@ public class Hook {
 
     }
 
+    public static String getPackageName(Activity activity) {
+        return activity.getApplicationContext().getPackageName();
+    }
+
     public static void beforeActivity$onCreate(Activity activity) { // NOSONAR
-        String packageName = activity.getPackageName();
+        String packageName = getPackageName(activity);
         Intent intent = new Intent(PreventIntent.ACTION_INCREASE_COUNTER, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
         intent.putExtra(PreventIntent.EXTRA_PID, Process.myPid());
         intent.putExtra(PreventIntent.EXTRA_UID, activity.getApplicationInfo().uid);
@@ -25,14 +29,14 @@ public class Hook {
     }
 
     public static void afterActivity$onDestroy(Activity activity) { // NOSONAR
-        String packageName = activity.getPackageName();
+        String packageName = getPackageName(activity);
         Intent intent = new Intent(PreventIntent.ACTION_DECREASE_COUNTER, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
         intent.putExtra(PreventIntent.EXTRA_PID, Process.myPid());
         sendBroadcast(activity, intent);
     }
 
     public static void beforeActivity$onRestart(Activity activity) { // NOSONAR
-        String packageName = activity.getPackageName();
+        String packageName = getPackageName(activity);
         Intent intent = new Intent(PreventIntent.ACTION_RESTART, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
         sendBroadcast(activity, intent);
     }
@@ -46,7 +50,7 @@ public class Hook {
             } else {
                 PreventLog.i("System.exit is called in activity, uid: " + uid);
             }
-            String packageName = activity.getPackageName();
+            String packageName = getPackageName(activity);
             Intent intent = new Intent(PreventIntent.ACTION_FORCE_STOP, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
             intent.putExtra(PreventIntent.EXTRA_UID, uid);
             sendBroadcast(activity, intent);
@@ -58,7 +62,7 @@ public class Hook {
     public static void afterActivity$moveTaskToBack(Activity activity, Boolean result) { // NOSONAR
         if (Boolean.TRUE.equals(result)) {
             PreventLog.i("moveTaskToBack: " + activity.getClass());
-            String packageName = activity.getPackageName();
+            String packageName = getPackageName(activity);
             Intent intent = new Intent(PreventIntent.ACTION_ACTIVITY_DESTROY, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
             sendBroadcast(activity, intent);
         }
@@ -66,7 +70,7 @@ public class Hook {
 
     public static void beforeActivity$startHomeActivityForResult(Activity activity) { // NOSONAR
         PreventLog.i("start home activity: " + activity.getClass());
-        String packageName = activity.getPackageName();
+        String packageName = getPackageName(activity);
         Intent intent = new Intent(PreventIntent.ACTION_ACTIVITY_DESTROY, Uri.fromParts(PreventIntent.SCHEME, packageName, null));
         sendBroadcast(activity, intent);
     }
