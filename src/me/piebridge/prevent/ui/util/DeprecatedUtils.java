93be839ff7f1d72f5ddccba116d7e0ab1a21814f
Liu DongMiao <liudongmiao@gmail.com>
2015-12-19 11:14:50 +0800

    suppress deprecation

--- src/me/piebridge/prevent/ui/util/DeprecatedUtils.java
+++ src/me/piebridge/prevent/ui/util/DeprecatedUtils.java
@@ -7,25 +7,28 @@ import android.preference.PreferenceActivity;
 /**
  * Created by thom on 15/10/13.
  */
-@SuppressWarnings("ALL")
 public class DeprecatedUtils {
 
     private DeprecatedUtils() {
 
     }
 
+    @SuppressWarnings("deprecation")
     public static CharSequence getClipboard(Context context) {
         return ((android.text.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE)).getText();
     }
 
+    @SuppressWarnings("deprecation")
     public static void setClipboard(Context context, CharSequence content) {
         ((android.text.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE)).setText(content);
     }
 
+    @SuppressWarnings("deprecation")
     public static void addPreferencesFromResource(PreferenceActivity activity, int preferencesResId) {
         activity.addPreferencesFromResource(preferencesResId);
     }
 
+    @SuppressWarnings("deprecation")
     public static Preference findPreference(PreferenceActivity activity, CharSequence key) {
         return activity.findPreference(key);
     }
